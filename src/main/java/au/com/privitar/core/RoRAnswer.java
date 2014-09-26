500242ac60692588706eaffa57d9e4f5ba3476b5
Andrew Todd <ajt.other@gmail.com>
2014-09-26 14:24:17 +1000

    Started writing basic logic.

--- src/main/java/au/com/privitar/core/RoRAnswer.java
+++ src/main/java/au/com/privitar/core/RoRAnswer.java
@@ -18,8 +18,31 @@ public class RoRAnswer {
 
         double answer = 1 + 2;
 
+        setup(memberId);
+        preparePersonalCalculationData();
+        prepareInvestmentCalculationData();
+
+
         System.out.println("answer is: " + answer);
 
         return answer;
+    }
+
+    /* This sets everything for the calculations. Includes any shared items. */
+    private void setup(int memberId) {
+
+        System.out.println("Running calculation for member: " + memberId);
+
+
+    }
+
+    /* This is the logic to prepare the data for personal calculation */
+    private void preparePersonalCalculationData() {
+
+    }
+
+    /* This is the logic required to both prepare the data and do the calculation. */
+    private void prepareInvestmentCalculationData() {
+        
     }
 }
